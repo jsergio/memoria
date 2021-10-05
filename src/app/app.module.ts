369a8653422c797373896a2de082aa580d96136d
jsergio7 <jsergiosette@gmail.com>
2021-10-05 16:23:49 -0300

    Novo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,25 @@ import { AppComponent } from './app.component';
 import { CartaComponent } from './components/template/carta/carta.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatCardModule} from '@angular/material/card'; 
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatGridListModule } from '@angular/material/grid-list';
 import { TelaComponent } from './components/template/tela/tela.component';
+import { BarraComponent } from './components/template/barra/barra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartaComponent,
-    TelaComponent
+    TelaComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
